679f98de5de6938bb913c73279b44828a870fa4a
Zhanna Skaraieva <i.m.p.h.e.l.l@gmail.com>
2025-05-23 05:14:42 +0300

    1

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import './Header.css';
+
+export function Header() {
+    return (
+        <header className="App-header">
+            <p>
+                Edit
+            </p>
+        </header>
+    
+    );
+}
